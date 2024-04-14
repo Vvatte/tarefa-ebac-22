2b91699dfaf01f4c0ad01bb65cd2a7be6bfe6c8d
Vvatte <vvatte@yahoo.com>
2024-04-14 13:50:26 -0300

    tarefa 22 primeira tentativa

---
+++ src/br/com/vvatte/Pessoa.java
@@ -0,0 +1,28 @@
+package br.com.vvatte;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Pessoa {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digiter um nome e o sexo(-m ou -f) da pessoa separados por traço, faça isso para tres pessoas");
+
+        String frase = entrada.nextLine().toLowerCase();
+
+        System.out.println(" ");
+        System.out.println("Você digitou:");
+        System.out.println(frase);
+
+        String[] arrayNomes = frase.split(",");
+
+        System.out.println(" ");
+        System.out.println("As pessoas do sexo feminino são:");
+        Stream<String> pessoas = List.of(arrayNomes).stream()
+                .filter(pessoa -> pessoa.endsWith("-f"));
+
+        pessoas.forEach(System.out::println);
+
+    }
+}
